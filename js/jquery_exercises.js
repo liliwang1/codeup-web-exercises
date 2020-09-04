086ce7d5ef1621e7b2962ee0e8774f668b87d436
liliwang1 <monsi0510@hotmail.com>
2020-09-04 14:40:19 -0500

    finished exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -9,6 +9,7 @@
         $('p').dblclick(function() {
             $(this).css('font-size', '18px');
         });
+
         $('li').hover(
             function() {
                 $(this).css('color', 'red');
