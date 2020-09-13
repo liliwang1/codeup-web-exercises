33f745ab24bcee3467158a71e37faeb2643b6eb4
liliwang <monsi0510@hotmail.com>
2020-09-13 01:59:44 -0500

    Update weather-map project

--- js/weather_map_main.js
+++ js/weather_map_main.js
@@ -1,23 +1,33 @@
 (function () {
     "use strict"
     $().ready(function () {
-        var dateSA = new Date();
-        var dSA = dateSA.toLocaleString("en-CA");
-        $('#local-info').html(`<div>San Antonio: </div>
-                                    <div>${dSA.slice(0, dSA.indexOf(','))}</div>
-                                    <div>${dSA.slice(dSA.indexOf(' '), dSA.length)}</div>`); // not displayed on webpage
+        mapboxgl.accessToken = MAPBOX_TOKEN;
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-98.5098047, 29.4642251],
+            zoom: 5
+        });
+
+        var marker = new mapboxgl.Marker({
+            draggable: true,
+            color: 'pink',
+            // scale: 1
+        })
+            .setLngLat([-98.4936, 29.4241])
+            .addTo(map);
 
         $('#citySearch').click(function (e) {
             e.preventDefault();
             var citySearch = $('#cityInput').val();
-            updateWeather(citySearch);
+            updateWeatherByLocation(citySearch);
             $('#cityInput').val('');
         })
 
-        function themeToggle(main, date1) {
+        function toggleTheme(main, date1) {
             var hourNow = date1.getHours();
             console.log(hourNow);
-            if (hourNow < 7 || hourNow > 18) {
+            if (hourNow < 6 || hourNow > 18) {
                 document.documentElement.style.setProperty('--background-img', 'url("../image/darkness.png")');
                 if (main === 'Clouds') {
                     $('#icon').html('<i class="fas fa-cloud-moon" style="font-size:1.8em"></i>');
@@ -51,21 +61,10 @@
             }
         }
 
-        // function navUpdate() {
-        // }
-
-        function updateWeather(q) {
-            $.ajax("http://api.openweathermap.org/data/2.5/weather?units=imperial", {
-                data: {
-                    APPID: OPEN_WEATHER_APPID,
-                    q: q
-                }
-            }).done(onSuccessCurrent);
-
-            function renderCurrent(data, dLocal) {
-                let html = `
+        function renderCurrent(data, dLocal, weekday) {
+            let html = `
                     <div class="mb-0">
-                        <h3 class="mb-0">${data.name}</h3>
+                        <h3 class="text-center mb-0">${data.name}</h3>
                         <div class="text-center">${data.sys.country}</div>
                     </div>
                     <div class="h6 mb-0 font-weight-bold">
@@ -82,40 +81,37 @@
                         <div class="text-wrap pb-3">Wind speed:  ${data.wind.speed} m/hr</div>
                     </div>
                     `;
-                $('#current').html(html);
-                $('#popover').click(function (e) {
-                    e.preventDefault();
-                    $('div#popover-box').html(`max-temp: ${data.main.temp_min} ℉<br>min-temp: ${data.main.temp_max} ℉`).toggleClass('invisible');
-                });
-                $('#date').html(`<span>${data.name}, ${data.sys.country}  
-                                      <span class=" ml-3 font-italic text-secondary">${dLocal.toLocaleString("en-CA")}</span>
+            $('#current').html(html);
+            $('#popover').click(function (e) {
+                e.preventDefault();
+                $('div#popover-box').html(`max-temp: ${data.main.temp_min} ℉<br>min-temp: ${data.main.temp_max} ℉`).toggleClass('invisible');
+            });
+            $('#date').html(`<span>${data.name}, ${data.sys.country}  
+                                      <span class=" ml-3 font-italic text-secondary">${dLocal.toLocaleString("en-CA").split(", ").reverse().join(" ")}, ${weekday}</span>
                                   </span>
                                  `);
-            }
+        }
 
-            function onSuccessCurrent(data, status, jqXhr) {
-                console.log("Request status: " + status);
-                var dLocal = new Date((data.dt + (data.timezone + 18000)) * 1000);
-                console.log(data, data.weather[0].main, data.dt, dLocal);
-                renderCurrent(data, dLocal);
-                themeToggle(data.weather[0].main, dLocal);
-            }
+        function onSuccessCurrent(data, status) {
+            console.log("Request status: " + status);
+            var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var dLocal = new Date((data.dt + (data.timezone + 18000)) * 1000);
+            console.log(data, data.weather[0].main, data.dt, dLocal);
+            var weekday = days[dLocal.getDay()];
+            renderCurrent(data, dLocal, weekday);
+            toggleTheme(data.weather[0].main, dLocal);
+            marker.setLngLat([data.coord.lon, data.coord.lat]);
+            map.setCenter([data.coord.lon, data.coord.lat]);
+        }
 
-            $.ajax("http://api.openweathermap.org/data/2.5/forecast?units=imperial", {
-                data: {
-                    APPID: OPEN_WEATHER_APPID,
-                    q: q,
-                }
-            }).done(onSuccessForecast);
-
-            function renderForecast(data) {
-                var html = "";
-                var list = data.list;
-                console.log(data.list);
-                for (let listItem of list) {
-                    if (listItem.dt_txt.includes("12:00:00")) {
-                        console.log(listItem);
-                        html += `
+        function renderForecast(data) {
+            var html = "";
+            var list = data.list;
+            console.log(data.list);
+            for (let listItem of list) {
+                if (listItem.dt_txt.includes("12:00:00")) {
+                    console.log(listItem);
+                    html += `
                             <li id="forecast-item" class="list-group-item d-flex flex-wrap align-items-center justify-content-between h6" data-toggle="collapse" data-target="#collapse-box">
                                 <sapn class="text-nowrap">${listItem.dt_txt.split(' ')[0]}</sapn>
                                 <img class="mx-auto" src="http://openweathermap.org/img/w/${listItem.weather[0].icon}.png">
@@ -134,23 +130,62 @@
                                 </span>
                             </p>
                             `;
-                    }
                 }
-                $('#forecast').html(html);
-                $('#cityName').html(`of  ${data.city.name}`);
             }
-
-            function onSuccessForecast(data, status, jqXhr) {
-                console.log("Request status: " + status);
-                console.log(data);
-                renderForecast(data);
-            }
+            $('#forecast').html(html);
+            $('#cityName').html(`of  ${data.city.name}`);
         }
 
-        updateWeather('San Antonio, US');
+        function onSuccessForecast(data, status) {
+            console.log("Request status: " + status);
+            console.log(data);
+            renderForecast(data);
+        }
+
+        function updateWeatherByLocation(q) {
+            $.ajax("http://api.openweathermap.org/data/2.5/weather?units=imperial", {
+                data: {
+                    APPID: OPEN_WEATHER_APPID,
+                    q: q
+                }
+            }).done(onSuccessCurrent);
+
+            $.ajax("http://api.openweathermap.org/data/2.5/forecast?units=imperial", {
+                data: {
+                    APPID: OPEN_WEATHER_APPID,
+                    q: q,
+                }
+            }).done(onSuccessForecast);
+        }
+
+        updateWeatherByLocation('San Antonio');
+
+        marker.on('dragend', updateWeatherByMarker);
+
+        function updateWeatherByMarker() {
+            map.setZoom(6);
+            var lngLat = marker.getLngLat();
+
+            $.ajax(`http://api.openweathermap.org/data/2.5/weather?lat=${lngLat.lat}&lon=${lngLat.lng}&units=imperial`, {
+                data: {
+                    APPID: OPEN_WEATHER_APPID,
+                }
+            }).done(onSuccessCurrent);
+
+            $.ajax(`http://api.openweathermap.org/data/2.5/forecast?lat=${lngLat.lat}&lon=${lngLat.lng}&units=imperial`, {
+                data: {
+                    APPID: OPEN_WEATHER_APPID,
+                }
+            }).done(onSuccessForecast);
+        }
 
         // var city = $('#dropdownCity').val();
         // $('#dropdownCity').on('change', updateWeather(city));
 
+        var dateSA = new Date();
+        var dSA = dateSA.toLocaleString("en-CA");
+        $('#local-info').html(`<div>San Antonio: </div>
+                                    <div>${dSA.slice(0, dSA.indexOf(','))}</div>
+                                    <div>${dSA.slice(dSA.indexOf(' '), dSA.length)}</div>`); // not displayed on webpage
     });
 })();
