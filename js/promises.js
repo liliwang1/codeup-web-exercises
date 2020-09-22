27442a47138110b51b5af6f2b62fc019fc9eac88
liliwang <monsi0510@hotmail.com>
2020-09-21 22:10:11 -0500

    Add finished exercise

--- js/promises.js
+++ js/promises.js
@@ -1,12 +1,59 @@
+let getDataLastCommit = (user) => {
+    let url = `https://api.github.com/users/${user}/events/public`;
+    return fetch(url, {headers: {'Authorization': `token ${GITHUB_API_TOKEN}`}})
+        .then(res => res.json())
+        .then(data => data.filter(event => event.type === "PushEvent"))
+        .then(event => event[0].created_at)
+        .catch(err => console.log(err));
+}
+
+console.log(getDataLastCommit('liliwang1'));
+
+// use DOM events to display on page
+document.getElementById('btn').addEventListener('click',
+    (e) => {
+    e.preventDefault();
+    let username = document.getElementById('username').value;
+    getDataLastCommit('username')
+        .then(date => {
+            document.getElementById('output').innerText = date;
+        })
+})
+
+
+fetch('https://api.github.com/users', {
+    headers: {
+        'Authorization': `token ${GITHUB_API_TOKEN}`
+    }
+}).then(response => response.json())
+    .then(users => {
+    users.forEach( userObj => {
+        lastCommit(userObj);
+        console.log(userObj);
+    })})
+    .catch(error => console.error(error));
+
+function lastCommit(user) {
+    return fetch(`https://api.github.com/users/${user}/events/public`)
+        .then(res => res.json())
+        .then(data => data)
+    // let newPromise = new Promise((resolve, reject) => {
+    //    resolve(user.login);
+    // });
+    // newPromise.then((res) => console.log(res));
+}
+
+
+
 function wait(num) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve();
+            resolve('resolved');
         }, num)
     })
 }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(1000).then((mesg) => console.log('You\'ll see this after 1 second' + mesg));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 function wait1(num) {
@@ -21,3 +68,6 @@ function wait1(num) {
 wait1(4000).then((mesg) => console.log(mesg));
 wait1(600).then((mesg) => console.log(mesg));
 // wait1(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+let randomNum = Math.floor(Math.random() * 8000);
+wait1(randomNum).then((mesg) => console.log(mesg));
